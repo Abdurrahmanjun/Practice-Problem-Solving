a8818de6e04ccce8d893162bb815d14183c837ba
Jun <abdurrahmanjun@gmail.com>
2023-03-09 02:59:17 +0700

    leetcode - rotate array

--- src/main/kotlin/leetcode/RotateArray.kt
+++ src/main/kotlin/leetcode/RotateArray.kt
@@ -2,18 +2,34 @@ package leetcode
 
 class RotateArray {
     fun solution(nums: IntArray, k:Int): IntArray {
-        var index = nums.size - 1 - k
-        for (i in nums.indices) {
-            if (index < k) {
-                val temp = nums[i]
-                nums[i] = nums[index]
-                nums[i] = temp
-                index++
-            } else {
-                break
-            }
-        }
+        val move = k % nums.size
+        println("move $move")
+
+        swap(nums,0,nums.lastIndex)
+        swap(nums,0,move - 1)
+        swap(nums,move,nums.lastIndex)
 
         return nums
     }
+
+    private fun swap(nums: IntArray, start:Int, end:Int) {
+        var mStart = start
+        var mEnd = end
+        if (nums.size > 1) {
+            while (mStart <= mEnd) {
+                println("start $mStart - end $mEnd")
+                val temp = nums[mStart]
+                nums[mStart] = nums[mEnd]
+                nums[mEnd] = temp
+                mStart++
+                mEnd--
+            }
+        }
+        println("nums ${nums.joinToString(", ")}")
+    }
+}
+
+fun main(args: Array<String>) {
+    val rotateArray = RotateArray()
+    rotateArray.solution(intArrayOf(1,2,3,4,5,6,7),3)
 }
